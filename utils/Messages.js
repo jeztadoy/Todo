7466f4d800d790346d0f2fb833e10af8041f96be
Tanzi Mubaroq <jeztadoy@gmail.com>
2018-07-04 08:27:24 +0700

    Add files via upload

---
+++ utils/Messages.js
@@ -0,0 +1,19 @@
+const messages = { 
+  EMAIL_BLANK: 'Email still blank',
+  PASSWORD_BLANK: 'Password still blank',
+  TITLE_BLANK: 'Title still blank',
+  DESCRIPTION_BLANK: 'Description still blank',
+  TODO_CREATE_SUCCESS: 'Success create new Todo',
+  TODO_UPDATE_SUCCESS: 'Success update Todo', TODO_DELETE_SUCCESS: 'Success delete Todo',
+  ACCOUNT_CREATE_SUCCESS: 'You have already registered',
+  EMAIL_FILL: 'Fill your email',
+  PASSWORD_FILL: 'Fill your password',
+  WELCOME_RNTODO: 'Welcome to RNTodo',
+  SIGN_OUT: 'You have already signed out',
+  TITLE_FILL: 'Fill the title',
+  DESCRIPTION_FILL: 'Fill the description',
+  FULLNAME_BLANK: 'Fullname still blank',
+  FULLNAME_FILL: 'Fill the fullname',
+}
+
+export default messages
